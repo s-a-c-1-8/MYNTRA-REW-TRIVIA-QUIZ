687aa9d378b15914c7299f580d5ed4ef1564861b
s-a-c-1-8 <sachindenki98@gmail.com>
2025-11-12 17:17:10 +0530

    ulr-test

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ import QuizGame from "./MyntraQuizGame";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<QuizGame key="" />} />
+      {/* Default route */}
+      <Route path="/" element={<QuizGame key="default" />} />
+
+      {/* Second route (separate instance) */}
       <Route path="/two" element={<QuizGame key="two" />} />
+
+      {/* Redirect any unknown path to home */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
