bb03c9a1039aea42ff6a6462f755586311b4a1be
s-a-c-1-8 <sachindenki98@gmail.com>
2025-11-12 17:23:28 +0530

    fixed-issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
 import QuizGame from "./MyntraQuizGame";
-
 export default function App() {
-  return (
-    <Routes>
-      {/* Default route */}
-      <Route path="/" element={<QuizGame key="default" />} />
-
-      {/* Second route (separate instance) */}
-      <Route path="/two" element={<QuizGame key="two" />} />
-
-      {/* Redirect any unknown path to home */}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  );
+  return <QuizGame />;
 }
